11870405cd670b00db6054043ec48d52a5229e8a
Dipak <kumardipak962@gmail.com>
2025-10-07 14:39:40 +0530

    First COmmit

--- src/main/java/com/pscs/moneyx/entity/RequestLog.java
+++ src/main/java/com/pscs/moneyx/entity/RequestLog.java
@@ -5,6 +5,7 @@ package com.pscs.moneyx.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,7 +23,9 @@ public class RequestLog {
 	private String channel;
 	private String username;
 	private String reqType;
+	@Column(length=4000)
 	private String request;
+	@Column(length=4000)
 	private String response;
 	private String responseCode;
 	private String responseMessage;
